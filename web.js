23fcaf084d02ec579cd2332ceab9a8944108b26c
killjoy-20 <killjoy.20@gmail.com>
2013-07-13 08:59:46 +0000

    Updated with index.html

--- web.js
+++ web.js
@@ -3,7 +3,9 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  String buf = fs.readFileSync("index.html");
+  Buffer buff = new Buffer(buf);
+  response.send(buff);
 });
 
 var port = process.env.PORT || 5000;
